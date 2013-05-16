8e7f569c6a1d33427c4775d13eb1f91ca2337153
Sho Kusano <rosylilly@aduca.org>
2013-05-16 20:31:05 +0900

    Refactor LunchMembers's spec

--- enumerable/spec/lunch_members_spec.rb
+++ enumerable/spec/lunch_members_spec.rb
@@ -4,12 +4,14 @@ require 'spec_helper'
 describe LunchMembers do
   describe 'shuffle' do
     let(:all_members) { %w[foo bar baz hoge piyo fuga moge gori] }
-    subject { LunchMembers.shuffle(all_members) }
+
+    subject(:shuffled) { LunchMembers.shuffle(all_members) }
+
     it 'returns 2 divided teams' do
-      subject.count.should == 2
+      shuffled.count.should == 2
 
       members = []
-      subject.each do |group|
+      shuffled.each do |group|
         group.count.should <= 4
         group.each do |member|
           members << member
