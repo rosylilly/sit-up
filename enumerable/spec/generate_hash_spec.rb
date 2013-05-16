6dd712bfc1b0b216fa3faa8673bb63e0aeffd042
Sho Kusano <rosylilly@aduca.org>
2013-05-16 20:29:48 +0900

    Refactor GenerateHash's spec

--- enumerable/spec/generate_hash_spec.rb
+++ enumerable/spec/generate_hash_spec.rb
@@ -2,21 +2,45 @@
 require 'spec_helper'
 describe GenerateHash do
   let(:expected_hash) do
-    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
+    {
+      'key1' => 'value1',
+      'key2' => 'value2',
+      'key3' => 'value3'
+    }
   end
 
   context 'when array that key, value, key, value... was given' do
-    subject { GenerateHash.by_array ['key1', 'value1', 'key2', 'value2', 'key3', 'value3'] }
-    it { should == expected_hash }
+    let(:argument) do
+      ['key1', 'value1', 'key2', 'value2', 'key3', 'value3']
+    end
+
+    subject(:by_array) { GenerateHash.by_array(argument) }
+
+    example do
+      expect(by_array).to eq expected_hash
+    end
   end
 
   context 'when array that pairs of key and value was given' do
-    subject { GenerateHash.by_nested_array [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']] }
-    it { should == expected_hash }
+    let(:argument) do
+      [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']]
+    end
+
+    subject(:by_nested_array) { GenerateHash.by_nested_array(argument) }
+
+    example do
+      expect(by_nested_array).to eq expected_hash
+    end
   end
 
   context 'when array of keys and array of values was given' do
-    subject { GenerateHash.by_keys_and_values ['key1', 'key2', 'key3'], ['value1', 'value2', 'value3'] }
-    it { should == expected_hash }
+    let(:keys) { ['key1', 'key2', 'key3'] }
+    let(:values) { ['value1', 'value2', 'value3'] }
+
+    subject(:by_keys_and_values) { GenerateHash.by_keys_and_values(keys, values)  }
+
+    example do
+      expect(by_keys_and_values).to eq expected_hash
+    end
   end
 end
